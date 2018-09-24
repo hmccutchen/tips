1f6781a5eddb7bdb147162eaa2aea7fab5faab50
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-09-24 12:10:20 -0400

    can search for restaurants by name, need to be able to add reviews to a res

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,32 @@
 class RestaurantsController < ApplicationController
+
+
+API_KEY = "mY9ou23owK6ViN4MQnkI1o-z7ksAicUDyV4Q_0Dq2Qm829nDuxlPrTQo-Vc5kU9jZlmdmoJ7p4o3Mabc9BYxPVpIKv3PnOfyCzU4S7r2SdxuJ4bE612sNNj4BEBvW3Yx"
+
+
   def index
+@results = params[:q]
+# @address = params[:a]
+
+http = Curl.get("https://api.yelp.com/v3/businesses/search?&term=#{@results}&location=New+York") do |http|
+  http.headers["Authorization"] = "Bearer #{API_KEY}"
+end
+ @r = JSON.parse(http.body_str)
+ p @r
+ # @res_name = @r["businesses"][0]["name"]
+ # @res_location = @r["businesses"][0]["location"]["display_address"]
+ # @res_rating = @r["businesses"][0]["rating"]
+ # @res_phone = @r["businesses"][0]["display_phone"]
+ # @res_price = @r["businesses"][0]["price"]
+ # @res_cat = @r["businesses"][0]["categories"][0]["title"]
+
   end
+
+def search; end
+
+
+
+
 
   def new
   end
@@ -10,4 +36,11 @@ class RestaurantsController < ApplicationController
 
   def edit
   end
+
+
+
+
+private
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  get 'restaurants/new'
-  get 'restaurants/show'
-  get 'restaurants/edit'
+
+root 'restaurants#index'
+
+resources :restaurants
+
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
