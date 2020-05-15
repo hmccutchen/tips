9bb4b644c34f7a0fc7b906f593335e1fcab8b22e
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2020-05-15 18:34:17 -0400

    refactor log in and sign up form

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,12 +1,1 @@
 
-
-
-
-window.onload = function(){
-  let scrollCircle = document.querySelector('.scroll-circle');
-  scrollCircle.classList.remove("hide-scroll");
-  scrollCircle.addEventListener('click',function(){
-  let navBar = document.querySelector('.navbar');
-  navBar.scrollIntoView({behavior: 'smooth', block: 'start'});
- })
-}
